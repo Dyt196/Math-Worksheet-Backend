41e688778c7daee20d68ed0e081864ea97f907e2
Dyt196 <m.ikhmalhidayat@gmail.com>
2025-07-04 04:17:18 +0800

    Update Import

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from 'express'
 import db from './database/db.js';
 import cors from 'cors';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 const app = express()
 app.use(cors());
