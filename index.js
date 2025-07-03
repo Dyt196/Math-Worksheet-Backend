e604531a5d9238eaacd157cf73b5ce30bd5951f0
Dyt196 <m.ikhmalhidayat@gmail.com>
2025-07-04 04:15:32 +0800

    Upload Frontend

--- index.js
+++ index.js
@@ -5,6 +5,16 @@ import cors from 'cors';
 const app = express()
 app.use(cors());
 app.use(express.json());
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname, 'frontend')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'frontend', 'index.html'));
+});
+
+
 const port = 3030
 
 app.get('/', (req, res) => {
