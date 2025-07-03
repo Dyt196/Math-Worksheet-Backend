801aa60cdc02a70bd1cbe9c3dc44cda05ac328d8
Dyt196 <m.ikhmalhidayat@gmail.com>
2025-07-04 04:25:19 +0800

    Update Frontenf and Path Regex

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 app.use(express.static(path.join(__dirname, 'frontend')));
-app.get('*', (req, res) => {
+app.get(/(.*)/, (req, res) => {
   res.sendFile(path.join(__dirname, 'frontend', 'index.html'));
 });
 
